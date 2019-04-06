74516fa73a33af67b487a9bd9e8f4e2d2625dce4
Shawn Presser <shawnpresser@gmail.com>
2019-04-06 17:34:02 -0500

    Use ExecuteString

--- deps/v8/src/bootstrapper.cc
+++ deps/v8/src/bootstrapper.cc
@@ -3847,6 +3847,112 @@ void Genesis::InitializeExperimentalGlobal() {
 #undef FEATURE_INITIALIZE_GLOBAL
 }
 
+#if 1
+# if defined(__IPHONEOS__)
+#  define HAX 1
+# endif
+#else
+# undef HAX
+#endif
+
+#if HAX
+
+// Extracts a C string from a V8 Utf8Value.
+const char* ToCString(const v8::String::Utf8Value& value) {
+  return *value ? *value : "<string conversion failed>";
+}
+
+
+void ReportException(v8::Isolate* isolate, v8::TryCatch* try_catch) {
+  v8::HandleScope handle_scope(isolate);
+  v8::String::Utf8Value exception(isolate, try_catch->Exception());
+  const char* exception_string = ToCString(exception);
+  v8::Local<v8::Message> message = try_catch->Message();
+  if (message.IsEmpty()) {
+    // V8 didn't provide any extra information about this error; just
+    // print the exception.
+    fprintf(stderr, "%s\n", exception_string);
+  } else {
+    // Print (filename):(line number): (message).
+    v8::String::Utf8Value filename(isolate,
+                                   message->GetScriptOrigin().ResourceName());
+    v8::Local<v8::Context> context(isolate->GetCurrentContext());
+    const char* filename_string = ToCString(filename);
+    int linenum = message->GetLineNumber(context).FromJust();
+    fprintf(stderr, "%s:%i: %s\n", filename_string, linenum, exception_string);
+    // Print line of source code.
+    v8::String::Utf8Value sourceline(
+        isolate, message->GetSourceLine(context).ToLocalChecked());
+    const char* sourceline_string = ToCString(sourceline);
+    fprintf(stderr, "%s\n", sourceline_string);
+    // Print wavy underline (GetUnderline is deprecated).
+    int start = message->GetStartColumn(context).FromJust();
+    for (int i = 0; i < start; i++) {
+      fprintf(stderr, " ");
+    }
+    int end = message->GetEndColumn(context).FromJust();
+    for (int i = start; i < end; i++) {
+      fprintf(stderr, "^");
+    }
+    fprintf(stderr, "\n");
+    v8::Local<v8::Value> stack_trace_string;
+    if (try_catch->StackTrace(context).ToLocal(&stack_trace_string) &&
+        stack_trace_string->IsString() &&
+        v8::Local<v8::String>::Cast(stack_trace_string)->Length() > 0) {
+      v8::String::Utf8Value stack_trace(isolate, stack_trace_string);
+      const char* stack_trace_string = ToCString(stack_trace);
+      fprintf(stderr, "%s\n", stack_trace_string);
+    }
+  }
+}
+
+// Executes a string within the current v8 context.
+bool ExecuteString(v8::Isolate* isolate, v8::Handle<v8::String> source,
+                   v8::Handle<v8::Value> name, bool print_result,
+                   bool report_exceptions, bool call_result) {
+  v8::HandleScope handle_scope(isolate);
+  v8::TryCatch try_catch(isolate);
+  v8::ScriptOrigin origin(name);
+  v8::Local<v8::Context> context(isolate->GetCurrentContext());
+  v8::Local<v8::Script> script;
+
+#if 0
+  v8::String::Utf8Value str(isolate, source);
+  if (call_result) str += "()";
+  const char* cstr = ToCString(str);
+  Handle<String> source2 =
+      isolate->factory()->NewStringFromUtf8(cstr).ToHandleChecked();
+#endif
+
+  if (!v8::Script::Compile(context, source, &origin).ToLocal(&script)) {
+    // Print errors that happened during compilation.
+    if (report_exceptions)
+      ReportException(isolate, &try_catch);
+    return false;
+  } else {
+    v8::Local<v8::Value> result;
+    if (!script->Run(context).ToLocal(&result)) {
+      assert(try_catch.HasCaught());
+      // Print errors that happened during execution.
+      if (report_exceptions)
+        ReportException(isolate, &try_catch);
+      return false;
+    } else {
+      assert(!try_catch.HasCaught());
+      if (print_result && !result->IsUndefined()) {
+        // If all went well and the result wasn't undefined then print
+        // the returned value.
+        v8::String::Utf8Value str(isolate, result);
+        const char* cstr = ToCString(str);
+        printf("%s\n", cstr);
+      }
+      return true;
+    }
+  }
+}
+#endif
+
+
 bool Bootstrapper::CompileExtraBuiltin(Isolate* isolate, int index) {
   HandleScope scope(isolate);
   Vector<const char> name = ExtraNatives::GetScriptName(index);
@@ -3856,8 +3962,18 @@ bool Bootstrapper::CompileExtraBuiltin(Isolate* isolate, int index) {
   Handle<Object> binding = isolate->extras_binding_object();
   Handle<Object> extras_utils = isolate->extras_utils_object();
   Handle<Object> args[] = {global, binding, extras_utils};
+#if HAX
+  Handle<String> script_name =
+      isolate->factory()->NewStringFromUtf8(name).ToHandleChecked();
+  return ExecuteString(
+    reinterpret_cast<v8::Isolate*>(isolate),
+    *reinterpret_cast<v8::Handle<v8::String>*>(&source_code),
+    *reinterpret_cast<v8::Handle<v8::String>*>(&script_name),
+    false, true, false);
+#else
   return Bootstrapper::CompileNative(isolate, name, source_code,
                                      arraysize(args), args, EXTENSION_CODE);
+#endif
 }
 
 
